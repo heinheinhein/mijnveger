3b6b5dd7b55ccaa0f97ce8e162018e9d5b4a5de7
hein <57179017+heinheinhein@users.noreply.github.com>
2024-08-17 01:38:04 +0200

    Als je verliest kijkt de clown (jij) nu naar hun eigen game

--- src/scripts/loseAnimation.ts
+++ src/scripts/loseAnimation.ts
@@ -1,15 +1,11 @@
 import { GameEndDetails } from "./types";
+import { toSvg } from "html-to-image";
+import Perspective from "perspectivets"
 import helicopterMp3 from "../media/funny/helicopter.mp3";
 import helicopterGif from "../media/funny/helicopter.gif";
 import clownGif from "../media/funny/clown.gif";
 import nukeMp3 from "../media/funny/nuke.mp3";
 import explosieGif from "../media/funny/explosie.gif";
-
-
-
-/// ik wil de mines pas revealen als de explosies komen:
-//   - mijnveger class aanpassen zodat revealmines een functie is nadat het spel klaar is
-//   - .mine css class aanpassen zodat deze pas zichtbaar is vanaf een bepaald moment
 
 
 export async function loseAnimation(event: CustomEvent | Event): Promise<void> {
@@ -18,11 +14,15 @@ export async function loseAnimation(event: CustomEvent | Event): Promise<void> {
     if (!("detail" in event)) return;
     const details = event.detail as GameEndDetails;
 
+    // maak alvast de achtergrond want dit duurt even
+    const background = createBackground(2);
+
+    // helicopter
     await helicopter(details.mineCells);
+
+    // update de achtergrond
+    document.body.style.backgroundImage = `url("${await background}")`;
 }
-
-
-
 
 async function helicopter(mines: HTMLTableCellElement[]): Promise<void> {
     // helicopter audio
@@ -83,9 +83,7 @@ async function helicopter(mines: HTMLTableCellElement[]): Promise<void> {
     // onthul alle mines
     showMines();
 
-    await sleep(1800);
-    // verander de achtergrond nadat de bommen zijn ontploft
-    document.body.style.backgroundImage = `url("${clownGif}")`;
+    await sleep(1400);
 }
 
 
@@ -103,4 +101,70 @@ function showMines(): void {
 export function hideMines(): void {
     document.documentElement.style.setProperty("--wrong-color", "var(--tile-color)");
     document.documentElement.style.setProperty("--mine-content", "''");
+}
+
+
+async function createBackground(depth: number): Promise<string> {
+    // de achtergrond afbeelding, bij de eerste loop bestaat deze nog niet, bij de tweede wordt deze op de achtergrond van de achtergrond gezet
+    let backgroundImage: string = "";
+
+    // begin alvast de clown afbeelding te laden
+    const clownImage = new Image();
+    clownImage.src = clownGif;
+
+
+    // hoe vaak we de achtergrond genereren
+    // als dit 1 keer is, bevat de achtergrond op het computerscherm van de clown geen mijnveger game, maar gewoon zwart; dat kunnen we niet hebben
+    for (let i = 0; i < depth; i++) {
+
+        // maak canvas
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        if (!ctx) throw new Error("clown ctx is null? waarom");
+
+        // resize het canvas
+        await clownImage.decode();
+        canvas.width = clownImage.width;
+        canvas.height = clownImage.height;
+
+        // als dit de eerste iteratie is gebruiken we de clownImage even als achtergrond
+        if (backgroundImage === "") {
+            ctx.drawImage(clownImage, 0, 0);
+            backgroundImage = canvas.toDataURL();
+        }
+
+        // neem een "screenshot" van hoe de pagina er nu uit ziet, met als achtergrond de clown afbeelding, ook al is dat nog niet hoe de pagina er uit ziet
+        // toSvg is het snelst trouwens, geen idee waarom maar is maar 600ms tegenover 900ms van toPng en toJpeg etc. 
+        const screenshot = await toSvg(document.body, {
+            style: {
+                // gewoon de url van clownImage hier werkt niet voor een of andere reden 
+                backgroundImage: `url("${backgroundImage}")`
+            }
+        });
+        if (!screenshot) throw new Error("screenshot nemen mislukt :(");
+        const screenshotImage = new Image();
+        screenshotImage.src = screenshot;
+
+        // plaats de screenshot op het canvas en positioneer hem zodat deze achter het scherm staat van de clown
+        await screenshotImage.decode();
+        const p = new Perspective(ctx, screenshotImage);
+        p.draw({
+            topLeftX: 13,
+            topLeftY: 134,
+            topRightX: 124,
+            topRightY: 125,
+            bottomRightX: 141,
+            bottomRightY: 216,
+            bottomLeftX: 34,
+            bottomLeftY: 239,
+        });
+
+        // plaats clown afbeelding (met transparant scherm) op canvas
+        ctx.drawImage(clownImage, 0, 0);
+
+        // zet backgroundImage naar de nieuwe achtergrond, zodat er nog een loop kan uitgevoerd worden waardoor we nog een niveau dieper gaan, of dat we deze kunnen returnen
+        backgroundImage = canvas.toDataURL();
+    }
+
+    return backgroundImage;
 }
